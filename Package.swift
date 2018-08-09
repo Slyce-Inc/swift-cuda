1419e94870bae7bd226f76ed7a4ffc662b76831c
Jason Allum <jallum@tonicdesign.com>
2018-08-09 07:00:46 -0400

    Switched to use newest available via pkg-config.

--- Package.swift
+++ Package.swift
@@ -2,12 +2,24 @@
 
 import PackageDescription
 
+#if os(Linux)
+import Foundation
+
+let fileManager = FileManager.default
+guard let pcFileToUse = (try! fileManager.contentsOfDirectory(atPath:"/usr/lib/pkgconfig")
+  .filter { $0.hasPrefix("cudart-") && $0.hasSuffix(".pc") }
+  .sorted()
+  .last) else {
+  fatalError("Unable to locate a .pc for cudart")
+}
+let versionToUse = NSURL(fileURLWithPath:pcFileToUse).deletingPathExtension!.lastPathComponent
+#else
+let versionToUse = "cudart"
+#endif
+
 let package = Package(
   name: "Clibcuda",
-  pkgConfig: "cudart-9.0",
-  providers: [
-    .apt(["cuda-cudart-dev-9-0"])
-  ],
+  pkgConfig: versionToUse,
   products: [ 
     .library(name: "Clibcuda", targets: ["Clibcuda"]) 
   ],
